68a074d84c005b9b1ad78a184747146117567a78
joloe <loe@uni-bonn.de>
2019-01-12 23:02:57 +0100

    * Reorganize the code * Add classification with SVM

---
+++ code/arg_parser.py
@@ -0,0 +1,74 @@
+import argparse
+import os
+import random
+import torch
+import torch.backends.cudnn as cudnn
+
+
+'''
+---------------------------
+Definition of the inputs
+---------------------------
+
+
+    dataroot - the path to the root of the dataset folder. We will talk more about the dataset in the next section
+    workers - the number of worker threads for loading the data with the DataLoader
+    batch_size - the batch size used in training. The DCGAN paper uses a batch size of 128
+    ImageSize - the spatial size of the images used for training. This implementation defaults to 64x64. If another size is desired, the structures of D and G must be changed. See here for more details
+    nc - number of color channels in the input images. For color images this is 3
+    nz - length of latent vector
+    ngf - relates to the depth of feature maps carried through the generator
+    ndf - sets the depth of feature maps propagated through the discriminator
+    num_epochs - number of training epochs to run. Training for longer will probably lead to better results but will also take much longer
+    lr - learning rate for training. As described in the DCGAN paper, this number should be 0.0002
+    beta1 - beta1 hyperparameter for Adam optimizers. As described in paper, this number should be 0.5
+    ngpu - number of GPUs available. If this is 0, code will run in CPU mode. If this number is greater than 0 it will run on that number of GPUs
+
+'''
+
+# add parser to specify parameters for the cgan
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', required=True, help='kitchen |imagenet | celebA')
+parser.add_argument('--dataroot', required=True, help='path to dataset')
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=0)
+parser.add_argument('--batchSize', type=int, default=128, help='input batch size')
+parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
+parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
+parser.add_argument('--ngf', type=int, default=64)
+parser.add_argument('--ndf', type=int, default=64)
+parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
+parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
+parser.add_argument('--cuda', action='store_true', help='enables cuda')
+parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
+parser.add_argument('--model', default='', help="path to model (to continue training)")
+parser.add_argument('--svm', default='', help="train or validate with svm")
+parser.add_argument('--outf', default='.', help='folder to output images and model checkpoints')
+parser.add_argument('--manualSeed', type=int, help='manual seed')
+
+opt = parser.parse_args()
+print(opt)
+
+# load the checkpoint if training should be continued
+if opt.model != '':
+    checkpoint = torch.load(opt.model)
+
+# make output dirs and files
+try:
+    os.makedirs(opt.outf)
+except OSError:
+    pass
+
+# Set random seem for reproducibility
+if opt.manualSeed is None:
+    opt.manualSeed = random.randint(1, 10000)
+
+#manualSeed = random.randint(1, 10000) # use if you want new results
+print("Random Seed: ", opt.manualSeed)
+random.seed(opt.manualSeed)
+torch.manual_seed(opt.manualSeed)
+
+# give warning if cuda isnt used but available
+cudnn.benchmark = True
+if torch.cuda.is_available() and not opt.cuda:
+    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
---
+++ code/main.py
@@ -0,0 +1,362 @@
+'''
+A dcgan on different datasets. 
+
+input discriminator: 3xndfxndf
+output discriminator: scalar prob. that the input is frim the real data distr.
+discriminator strucutre: The discriminator is made up of strided convolution layers, batch norm layers, and LeakyReLU activations.
+
+input generator: latent vaector z, drawn from standard gaussian.
+output generator: 3xngfxngf RGB image
+generator structure: The generator is comprised of convolutional-transpose layers, batch norm layers, and ReLU activations.
+
+Why do we use strided conv-transpose? 
+The strided conv-transpose layers allow the latent vector to be transformed into a volume with the same shape as an image.
+'''
+# load libraries
+from __future__ import print_function
+#%matplotlib inline
+import arg_parser
+import dcgan
+import os
+import random
+import torch
+
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
+from sklearn import svm
+
+opt = arg_parser.opt
+
+# Size of z latent vector (i.e. size of generator input)
+nz = int(opt.nz)
+
+# Size of feature maps in generator
+ngf = int(opt.ngf)
+
+# Size of feature maps in discriminator
+ndf = int(opt.ndf)
+
+# Number of GPUs available. Use 0 for CPU mode.
+ngpu = int(opt.ngpu)
+
+'''
+Load the data and transform it to tensor dataset. 
+Create a pytorch dataloader and decide on which device we want to run the code.
+To test the settings, plot some training images. 64 in SUM.
+'''
+
+# We can use an image folder dataset the way we have it setup.
+# Create the dataset
+
+if opt.dataset in ['imagenet','celebA']:
+    # folder dataset
+    dataset = dset.ImageFolder(root=opt.dataroot,
+                               transform=transforms.Compose([
+                                   transforms.Resize(opt.imageSize),
+                                   transforms.CenterCrop(opt.imageSize),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                               ]))
+    nc=3
+elif opt.dataset == 'kitchen':
+    dataset = dset.LSUN(root=opt.dataroot, classes=['kitchen_train'],
+                        transform=transforms.Compose([
+                            transforms.Resize(opt.imageSize),
+                            transforms.CenterCrop(opt.imageSize),
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                        ]))
+    nc=3
+
+# Create the dataloader
+assert dataset
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
+                                         shuffle=True, num_workers=int(opt.workers))
+
+# Decide which device we want to run on
+device = torch.device("cuda:0" if opt.cuda else "cpu")
+print(device)
+# Plot some training images
+real_batch = next(iter(dataloader))
+plt.figure(figsize=(8,8))
+plt.axis("off")
+plt.title("Training Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+
+'''
+---------------------------
+weight initialization
+---------------------------
+
+From the DCGAN paper, the authors specify that all model weights shall be randomly initialized from a Normal distribution with mean=0, stdev=0.2. 
+The weights_init function takes an initialized model as input and reinitializes all convolutional, convolutional-transpose, and batch normalization layers to meet this criteria. 
+This function is applied to the models immediately after initialization.
+
+'''
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+'''
+Create a generator from the dcgan class. Load a checkpoint if it is available.
+'''
+
+
+# Create the generator
+netG = dcgan.Generator(ngpu).to(device)
+
+# Handle multi-gpu if desired
+if (device.type == 'cuda') and (ngpu > 1):
+    netG = nn.DataParallel(netG, list(range(ngpu)))
+
+# Apply the weights_init function to randomly initialize all weights
+#  to mean=0, stdev=0.2.
+netG.apply(weights_init)
+
+# load the model to continue training
+if opt.model != '':
+    netG.load_state_dict(arg_parser.checkpoint['netG'])
+    #netG.eval()
+
+# Print the model
+print(netG)
+
+# Create the Discriminator
+netD = dcgan.Discriminator(ngpu).to(device)
+
+# Handle multi-gpu if desired
+if (device.type == 'cuda') and (ngpu > 1):
+    netD = nn.DataParallel(netD, list(range(ngpu)))
+
+# Apply the weights_init function to randomly initialize all weights
+#  to mean=0, stdev=0.2.
+netD.apply(weights_init)
+
+# load the model to continue training
+
+if opt.model != '':
+    netD.load_state_dict(arg_parser.checkpoint['netD'])
+    #netD.eval()
+
+# Print the model
+print(netD)
+
+'''
+---------------------------
+loss function and optimizers
+---------------------------
+We will use the Binary Cross Entropy loss (BCELoss).
+this function provides the calculation of both log components in the objective function (i.e. log(D(x)) and log(1−D(G(z)))). 
+Next, we define our real label as 1 and the fake label as 0. These labels will be used when calculating the losses of D and G, and this is also the convention used in the original GAN paper. 
+Finally, we set up two separate optimizers, one for D and one for G. As specified in the DCGAN paper, both are Adam optimizers with learning rate 0.0002 and Beta1 = 0.5. For keeping track of the generator’s learning progression,
+we will generate a fixed batch of latent vectors that are drawn from a Gaussian distribution (i.e. fixed_noise) . In the training loop, we will periodically input this fixed_noise into G, and over the iterations we will see images form out of the noise.
+'''
+
+# Initialize BCELoss function
+criterion = nn.BCELoss()
+
+# Create batch of latent vectors that we will use to visualize
+#  the progression of the generator
+fixed_noise = torch.randn(opt.batchSize, nz, 1, 1, device=device)
+
+# Establish convention for real and fake labels during training
+real_label = 1
+fake_label = 0
+
+# Setup Adam optimizers for both G and D
+optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+
+if opt.model != '':
+    optimizerD.load_state_dict(arg_parser.checkpoint['optimizerD'])
+    optimizerG.load_state_dict(arg_parser.checkpoint['optimizerG'])
+
+
+'''
+---------------------------
+training the network
+---------------------------
+
+Part 1 - Train the Discriminator
+
+Recall, the goal of training the discriminator is to maximize the probability of correctly classifying a given input as real or fake. In terms of Goodfellow, 
+we wish to “update the discriminator by ascending its stochastic gradient”. Practically, we want to maximize log(D(x))+log(1−D(G(z))).
+Due to the separate mini-batch suggestion from ganhacks, we will calculate this in two steps. First, we will construct a batch of real samples from the training set, 
+forward pass through D, calculate the loss (log(D(x))), then calculate the gradients in a backward pass. Secondly, we will 
+construct a batch of fake samples with the current generator, forward pass this batch through D, calculate the loss (log(1−D(G(z)))), 
+and accumulate the gradients with a backward pass. Now, with the gradients accumulated from both the all-real and all-fake batches, we call a step of the Discriminator’s optimizer.
+
+Part 2 - Train the Generator
+
+As stated in the original paper, we want to train the Generator by minimizing log(1−D(G(z)))
+in an effort to generate better fakes. As mentioned, this was shown by Goodfellow to not provide 
+sufficient gradients, especially early in the learning process. As a fix, we instead wish to maximize log(D(G(z))). In the code we accomplish this by: 
+classifying the Generator output from Part 1 with the Discriminator, computing G’s loss using real labels as GT, computing G’s gradients in a backward pass, 
+and finally updating G’s parameters with an optimizer step. It may seem counter-intuitive to use the real labels as GT labels for the loss function,
+but this allows us to use the log(x) part of the BCELoss (rather than the log(1−x) part) which is exactly what we want.
+'''
+
+# Training Loop
+
+# Lists to keep track of progress
+img_list = []
+
+start_epoch = 0
+if opt.model != '':
+    start_epoch = checkpoint['epoch']+1
+    iters = checkpoint['iters']
+    G_losses = checkpoint['G_losses']
+    D_losses = checkpoint['D_losses']
+else :
+    G_losses = []
+    D_losses = []
+    iters = 0
+    
+print("Starting Training Loop...")
+# For each epoch
+for epoch in range(start_epoch,opt.niter):
+    # For each batch in the dataloader
+    for i, data in enumerate(dataloader, 0):
+
+        ############################
+        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        ###########################
+        ## Train with all-real batch
+        netD.zero_grad()
+        # Format batch
+        real_cpu = data[0].to(device)
+        b_size = real_cpu.size(0)
+        label = torch.full((b_size,), real_label, device=device)
+        # Forward pass real batch through D
+        output = netD(real_cpu).view(-1)
+        # Calculate loss on all-real batch
+        errD_real = criterion(output, label)
+        # Calculate gradients for D in backward pass
+        errD_real.backward()
+        D_x = output.mean().item()
+
+        ## Train with all-fake batch
+        # Generate batch of latent vectors
+        noise = torch.randn(b_size, nz, 1, 1, device=device)
+        # Generate fake image batch with G
+        fake = netG(noise)
+        label.fill_(fake_label)
+        # Classify all fake batch with D
+        output = netD(fake.detach()).view(-1)
+        # Calculate D's loss on the all-fake batch
+        errD_fake = criterion(output, label)
+        # Calculate the gradients for this batch
+        errD_fake.backward()
+        D_G_z1 = output.mean().item()
+        # Add the gradients from the all-real and all-fake batches
+        errD = errD_real + errD_fake
+        # Update D
+        optimizerD.step()
+
+        ############################
+        # (2) Update G network: maximize log(D(G(z)))
+        ###########################
+        netG.zero_grad()
+        label.fill_(real_label)  # fake labels are real for generator cost
+        # Since we just updated D, perform another forward pass of all-fake batch through D
+        output = netD(fake).view(-1)
+        # Calculate G's loss based on this output
+        errG = criterion(output, label)
+        # Calculate gradients for G
+        errG.backward()
+        D_G_z2 = output.mean().item()
+        # Update G
+        optimizerG.step()
+
+        # Output training stats
+        if i % 50 == 0:
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                  % (epoch, opt.niter, i, len(dataloader),
+                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+
+        # Save Losses for plotting later
+        G_losses.append(errG.item())
+        D_losses.append(errD.item())
+
+        # Check how the generator is doing by saving G's output on fixed_noise
+        if (iters % 500 == 0) or ((epoch == opt.niter-1) and (i == len(dataloader)-1)):
+            with torch.no_grad():
+                fake = netG(fixed_noise).detach().cpu()
+                vutils.save_image(real_cpu,
+                    '%s/img/samples/real_samples.png' % opt.outf,
+                    normalize=True)
+                vutils.save_image(fake.detach(),
+                    '%s/img/samples/fake_samples_epoch_%03d.png' % (opt.outf, epoch),
+                normalize=True)
+            img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+
+        iters += 1
+    # do checkpointing
+    torch.save({'netG': netG.state_dict(), 'netD': netD.state_dict(),'optimizerG': optimizerG.state_dict(), 'optimizerD': optimizerD.state_dict(), 'epoch': epoch, 'iters': iters , 'G_losses': G_losses, 'D_losses':D_losses}, '%s/models/model_%d.pth' % (opt.outf, epoch))
+
+
+# Generate a SVM. This SVM should use output of the conv2d layers as input and do classification
+
+class SVM():
+    def __init__(self):
+        super(SVM, self).__init__()
+        self.lin_clf = svm.LinearSVC()   
+    def forward(self, input, classes):
+        return self.lin_clf(input,classes)
+    def predict(self,test):
+        return self.lin_clf.predict(test)
+    def accuracy(self,test,true_labels):
+        return self.lin_clf.score(test,true_labels)
+
+# plot the losses of the generator and the discriminator
+plt.figure(figsize=(10,5))
+plt.title("Generator and Discriminator Loss During Training")
+plt.plot(G_losses,label="G")
+plt.plot(D_losses,label="D")
+plt.xlabel("iterations")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+plt.savefig('%s/img/plots/losses_epoch_%03d.png' % (opt.outf,epoch))
+
+#%%capture
+'''
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=15, metadata=dict(artist='dcgan'), bitrate=1800)
+fig = plt.figure(figsize=(8,8))
+plt.axis("off")
+ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+ani.save('%s/plots/training_progress.mp4' % (opt.outf),writer=writer)
+HTML(ani.to_jshtml())
+'''
+# Grab a batch of real images from the dataloader
+real_batch = next(iter(dataloader))
+
+# Plot the real images
+plt.figure(figsize=(15,15))
+plt.subplot(1,2,1)
+plt.axis("off")
+plt.title("Real Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
+# Plot the fake images from the last epoch
+plt.subplot(1,2,2)
+plt.axis("off")
+plt.title("Fake Images")
+plt.imshow(np.transpose(img_list[-1],(1,2,0)))
+plt.show()
+plt.savefig('%s/img/samples/real_vs_fake_images_epoch_%03d.png' % (opt.outf,epoch))
