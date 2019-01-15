7939755716b2435378b9c8c0a1cc8a90d714bb38
Loevenich Johannes <j.loevenich@enbw.com>
2019-01-15 15:18:54 +0100

    0-dim err bugfix

--- code/main.py
+++ code/main.py
@@ -308,11 +308,11 @@ for epoch in range(start_epoch,opt.niter):
         if i % 50 == 0:
             print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
                   % (epoch, opt.niter, i, len(dataloader),
-                     errD.data[0], errG.data[0], D_x, D_G_z1, D_G_z2))
+                     errD.item(), errG.data[0].item(), D_x, D_G_z1, D_G_z2))
 
         # Save Losses for plotting later
-        G_losses.append(errG.data[0])
-        D_losses.append(errD.data[0])
+        G_losses.append(errG.item())
+        D_losses.append(errD.item())
 
         # Check how the generator is doing by saving G's output on fixed_noise
         if (iters % 500 == 0) or ((epoch == opt.niter-1) and (i == len(dataloader)-1)):
