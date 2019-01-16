9611750aa0ffa7bf04305b7f0c79b1a3cd78704c
Loevenich Johannes <j.loevenich@enbw.com>
2019-01-16 18:35:01 +0100

    minor changes

--- code/classifier.py
+++ code/classifier.py
@@ -40,6 +40,16 @@ if __name__ == '__main__':
         if (device.type == 'cuda') and (opt.ngpu > 1):
             netD = nn.DataParallel(netD, list(range(opt.ngpu)))
 
+        def weights_init(m):
+            classname = m.__class__.__name__
+            if classname.find('Conv') != -1:
+                nn.init.normal_(m.weight.data, 0.0, 0.02)
+            elif classname.find('BatchNorm') != -1:
+                nn.init.normal_(m.weight.data, 1.0, 0.02)
+                nn.init.constant_(m.bias.data, 0)
+        
+        netD.apply(weights_init)
+
         # Load the Generator from the model 
         if opt.model == '':
             print('------- This code assumes that you load a model of a Generator -------')
